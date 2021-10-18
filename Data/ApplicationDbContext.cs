c768dfe04d963934b1fa1d3b724140bd574a6ecd
hnisar01 <72013214+hnisar01@users.noreply.github.com>
2021-10-18 15:40:38 +0100

    Draft for movies

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ASP_Razor_Movies.Models;
 
 namespace ASP_Razor_Movies.Data
 {
@@ -12,5 +13,6 @@ namespace ASP_Razor_Movies.Data
             : base(options)
         {
         }
+        public DbSet<ASP_Razor_Movies.Models.Movie> Movie { get; set; }
     }
 }
